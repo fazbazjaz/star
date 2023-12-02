d70c4e97af4f51fc7841b1e4f04d10b882d8a2c8
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-12-02 19:22:03 +0000

    additions

---
+++ client/src/api/getQuestionsByCursor.js
@@ -0,0 +1,17 @@
+const getQuestionsByCursor = async (cursor) => {
+  const response = await fetch(
+    `${
+      import.meta.env.VITE_SERVER_URL
+    }/api/questions/infinite?cursor=${cursor}`,
+    { credentials: "include" }
+  );
+  // console.log("getQuestionsByCursor response:", response);
+  if (!response.ok) {
+    throw new Error("getQuestionsByCursor failed");
+  }
+  const data = await response.json();
+  // console.log("getQuestionsByCursor data:", data);
+  return data;
+};
+
+export default getQuestionsByCursor;
