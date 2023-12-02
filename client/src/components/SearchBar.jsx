35fa8491f9566bae88e66f7f1297e71b62a178de
softacoder <113185323+softacoder@users.noreply.github.com>
2023-12-02 12:12:02 +0000

    Commit updates
    databaseschema needs mod

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,68 +1,101 @@
-// version pasted in from internet to modify
+// version1 my code
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+// import { debouncedSearchQuestions } from "../helpers/questions";
+import { debouncedSearchQuestions } from "../questions";
 
-import { useState, useEffect } from "react";
-import axios from "axios";
-import styles from "./SearchBar.module.css";
+const Question = () => {
+  const [searchResult, setSearchResult] = useState([]);
 
-const SearchBar = ({ onSearch }) => {
-  const [value, setValue] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [hideSuggestions, setHideSuggestions] = useState(true);
-
-  const fetchData = async () => {
+  const handleSearch = async (searchValue) => {
     try {
-      const { data } = await axios.get(
-        `https://dummyjson.com/products/search?q=${value}`
-      );
-
-      setSuggestions(data.products);
+      const result = await debouncedSearchQuestions(searchValue);
+      setSearchResult(result);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
-  };
-
-  useEffect(() => {
-    if (value.trim() === "") {
-      setHideSuggestions(true);
-      return;
-    }
-
-    const timer = setTimeout(() => {
-      fetchData();
-      setHideSuggestions(false);
-    }, 1000); // Adjust the delay as needed
-
-    return () => clearTimeout(timer);
-  }, [value]);
-
-  const handleSelect = (title) => {
-    setValue(title);
-    setHideSuggestions(true);
-    onSearch(title); // Pass the selected value to the parent component
   };
 
   return (
-    <div className={styles.container}>
-      <input
-        type="text"
-        className={styles.textbox}
-        placeholder="Search data..."
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <div
-        className={`${styles.suggestions} ${hideSuggestions && styles.hidden}`}>
-        {suggestions.map((suggestion) => (
-          <div
-            key={suggestion.title}
-            className={styles.suggestion}
-            onClick={() => handleSelect(suggestion.title)}>
-            {suggestion.title}
-          </div>
-        ))}
-      </div>
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      {/* Display your search results, e.g., using Result component */}
+      {searchResult.map((result) => (
+        // Render your search result components here
+        // Example: <Result key={result.id} {...result} />
+        <div key={result.id}>{result.question}</div>
+      ))}
+      {/* Add your existing Question component code here */}
     </div>
   );
 };
 
-export default SearchBar;
+export default Question;
+
+// version pasted in from internet to modify
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import styles from "./SearchBar.module.css";
+
+// const SearchBar = ({ onSearch }) => {
+//   const [value, setValue] = useState("");
+//   const [suggestions, setSuggestions] = useState([]);
+//   const [hideSuggestions, setHideSuggestions] = useState(true);
+
+//   const fetchData = async () => {
+//     try {
+//       const { data } = await axios.get(
+//         `https://dummyjson.com/products/search?q=${value}`
+//       );
+
+//       setSuggestions(data.products);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (value.trim() === "") {
+//       setHideSuggestions(true);
+//       return;
+//     }
+
+//     const timer = setTimeout(() => {
+//       fetchData();
+//       setHideSuggestions(false);
+//     }, 1000); // Adjust the delay as needed
+
+//     return () => clearTimeout(timer);
+//   }, [value]);
+
+//   const handleSelect = (title) => {
+//     setValue(title);
+//     setHideSuggestions(true);
+//     onSearch(title); // Pass the selected value to the parent component
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <input
+//         type="text"
+//         className={styles.textbox}
+//         placeholder="Search data..."
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//       />
+//       <div
+//         className={`${styles.suggestions} ${hideSuggestions && styles.hidden}`}>
+//         {suggestions.map((suggestion) => (
+//           <div
+//             key={suggestion.title}
+//             className={styles.suggestion}
+//             onClick={() => handleSelect(suggestion.title)}>
+//             {suggestion.title}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SearchBar;
--- server/src/helpers/questions.ts
+++ server/src/helpers/questions.ts
@@ -1,30 +1,198 @@
-// version1 searchbar 
+// version2 my code
+import { database } from "../database/connection";
+import { questions, answers, comments } from "../database/schema";
+import { eq, and } from "drizzle-orm";
+import { debounce } from "lodash";
 
-import React, { useState } from 'react';
-import SearchBar from './SearchBar';
-import Result from './Result'; // Import your Result component
-
-const Question = () => {
-  const [searchResult, setSearchResult] = useState(null);
-
-  const handleSearch = (searchValue) => {
-    // Perform the search logic based on the selected value
-    // Set the search result to update the UI
-    // Example: setSearchResult(searchValue);
-  };
-
-  return (
-    <div>
-      <SearchBar onSearch={handleSearch} />
-      {searchResult && <Result {...searchResult} />}
-      {/* Add your existing Question component code here */}
-    </div>
-  );
+export const getAllQuestions = async () => {
+  return await database.select().from(questions);
 };
 
-export default Question;
+export const getOneQuestion = async (questionId: number) => {
+  return await database
+    .select()
+    .from(questions)
+    .where(eq(questions.id, questionId));
+};
 
+export const getOneQuestionWithAnswers = async (questionId: number) => {
+  return await database.query.questions.findMany({
+    with: {
+      answers: true
+    },
+    where: eq(questions.id, questionId)
+  });
+};
 
+export const getOneQuestionWithAnswersAndComments = async (
+  questionId: number
+) => {
+  return await database.query.questions.findMany({
+    with: {
+      answers: {
+        with: {
+          comments: true
+        }
+      }
+    },
+    where: eq(questions.id, questionId)
+  });
+};
+
+export const getAllQuestionsByUser = async (userId: number) => {
+  return await database
+    .select()
+    .from(questions)
+    .where(eq(questions.userId, userId));
+};
+
+export const createQuestion = async (userId: number, question: string) => {
+  return await database
+    .insert(questions)
+    .values({ userId, question })
+    .returning();
+};
+
+export const editQuestion = async (questionId: number, question: string) => {
+  return await database
+    .update(questions)
+    .set({ question, updatedAt: new Date(Date.now()) })
+    .where(eq(questions.id, questionId))
+    .returning();
+};
+
+export const deleteQuestion = async (questionId: number) => {
+  return await database
+    .delete(questions)
+    .where(eq(questions.id, questionId))
+    .returning();
+};
+
+export const getAnswer = async (questionId: number, answerId: number) => {
+  return await database
+    .select()
+    .from(questions)
+    .innerJoin(answers, eq(questions.id, answers.questionId))
+    .where(and(eq(questions.id, questionId), eq(answers.id, answerId)));
+};
+
+export const createAnswer = async (
+  userId: number,
+  questionId: number,
+  situation: string,
+  task: string,
+  action: string,
+  result: string
+) => {
+  return await database
+    .insert(answers)
+    .values({ userId, questionId, situation, task, action, result })
+    .returning();
+};
+
+export const editAnswer = async (
+  questionId: number,
+  answerId: number,
+  situation: string,
+  task: string,
+  action: string,
+  result: string
+) => {
+  return await database
+    .update(answers)
+    .set({ situation, task, action, result, updatedAt: new Date(Date.now()) })
+    .where(and(eq(answers.questionId, questionId), eq(answers.id, answerId)))
+    .returning();
+};
+
+export const deleteAnswer = async (questionId: number, answerId: number) => {
+  return await database
+    .delete(answers)
+    .where(and(eq(answers.id, answerId), eq(answers.questionId, questionId)))
+    .returning();
+};
+
+export const createComment = async (
+  userId: number,
+  answerId: number,
+  comment: string
+) => {
+  return await database
+    .insert(comments)
+    .values({ userId, answerId, comment })
+    .returning();
+};
+
+export const editComment = async (
+  answerId: number,
+  commentId: number,
+  comment: string
+) => {
+  return await database
+    .update(comments)
+    .set({ comment })
+    .where(and(eq(comments.id, commentId), eq(comments.answerId, answerId)))
+    .returning();
+};
+
+export const deleteComment = async (answerId: number, commentId: number) => {
+  return await database
+    .delete(comments)
+    .where(and(eq(comments.id, commentId), eq(comments.answerId, answerId)))
+    .returning();
+};
+
+// export const searchQuestions = async (query: string) => {
+//   if (!query.trim()) {
+//     return []; // Return an empty array for an empty query
+//   }
+
+//   return await database
+//     .select()
+//     .from(questions)
+//     .where(questions.question.toLowerCase().like(`%${query.toLowerCase()}%`));
+// };
+
+export const searchQuestions = async (query: string) => {
+  if (!query.trim()) {
+    return []; // Return an empty array for an empty query
+  }
+
+  const queryLowerCase = query.toLowerCase(); // Convert query to lowercase
+
+  return await database
+    .select()
+    .from(questions)
+    .where(questions.question.toLowerCase().like(`%${queryLowerCase}%` as any));
+};
+
+export const debouncedSearchQuestions = debounce(searchQuestions, 1000); // Adjust the delay as needed
+
+// version1 searchbar
+
+// import { useState } from 'react';
+// import SearchBar from './SearchBar';
+// import Result from './Result'; // Import your Result component
+
+// const Question = () => {
+//   const [searchResult, setSearchResult] = useState(null);
+
+//   const handleSearch = (searchValue) => {
+//     // Perform the search logic based on the selected value
+//     // Set the search result to update the UI
+//     // Example: setSearchResult(searchValue);
+//   };
+
+//   return (
+//     <div>
+//       <SearchBar onSearch={handleSearch} />
+//       {searchResult && <Result {...searchResult} />}
+//       {/* Add your existing Question component code here */}
+//     </div>
+//   );
+// };
+
+// export default Question;
 
 // original version
 // import { database } from "../database/connection";
